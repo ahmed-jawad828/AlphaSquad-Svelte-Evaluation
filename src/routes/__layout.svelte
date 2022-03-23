131a6594bf466eecc2d8a93411fc4fa0e9baf719
stand8dev <alphasquad@stand8.io>
2022-03-24 02:01:33 +0500

    Simple design added - Functionality needs to be implemented

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,23 @@
 <Header />
 
 <main>
+
+  <section>
+    <div class="bg-red-700 flex justify-center items-center">
+      <div class="grid grid-cols-2 ">
+        <div class=''>
+          <form on:submit>
+            <textarea
+            class=" px-3 py-2 text-gray-500 w-[500px] border-2 rounded-lg"
+            rows="6"
+            placeholder="Paste your JSON code here"            
+            />           
+          </form>
+        </div>
+        <div class='px-3 py-2 text-gray-700 border-2 w-[500px] rounded-lg'>asdasdadasdasdasdas</div>
+      </div>
+    </div>
+  </section>
 	<!-- <slot /> -->
 </main>
 
