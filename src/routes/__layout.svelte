b44b973d36c35c828453396c8ea667c2b7bbcf5e
ahmed-jawad828 <ahmedjawad401@gmail.com>
2022-03-28 15:02:12 +0500

    UX improved

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,22 +2,12 @@
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
 	import { afterUpdate } from 'svelte';
-	let JsonObject = 'asdas';
+	let JsonObject = '';
 
 	let isJsonObjectValid = false;
 	const handleJsonChange = (e) => {
 		JsonObject = e.target.value;
-		console.log(
-			'🚀 ~ file: __layout.svelte ~ line 10 ~ handleJsonChange ~ JsonObject',
-			JsonObject[0]
-		);
-		// const result = JsonObject.filter((word, index) => word[index] != '[');
-
-		// console.log('🚀 ~ file: __layout.svelte ~ line 15 ~ handleJsonChange ~ result', result);
-		// const JSON = JsonObject.filter();
-
 		let temp = [];
-
 		for (let i = 0; i < JsonObject.length; i++) {
 			if (JsonObject[i] === '[' || JsonObject[i] === ']') {
 				console.log('first');
@@ -26,7 +16,14 @@
 				temp.push(JsonObject[i]);
 			}
 		}
+		// console.log('🚀 ~ file: __layout.svelte ~ line 20 ~ handleJsonChange ~ temp', temp);
 		// JsonObject = temp;
+		let dummyJSON = JSON.stringify(temp);
+		let JSONStringObject = JSON.parse(dummyJSON);
+		console.log(
+			'🚀 ~ file: __layout.svelte ~ line 22 ~ handleJsonChange ~ dummyJSON',
+			JSONStringObject
+		);
 		try {
 			let JSONString = JSON.parse(JsonObject);
 			if (
@@ -59,27 +56,69 @@
 <main>
 	<section>
 		<div class="flex justify-center items-center">
-			<div class="grid grid-cols-2 mx-auto  w-10/12 ">
-				<form on:submit class="w-full text-gray-700 border-2 rounded-lg">
+			<div class="grid grid-cols-2 mx-auto  w-11/12 gap-x-3 ">
+				<form on:submit class="w-full text-gray-700 pb-2 ">
 					<textarea
-						class="text-gray-500 text-sm w-full placeholder:py-2 placeholder:px-3 py-2 placeholder:text-xs outline-none"
-						rows="6"
+						class="text-gray-500 text-sm w-full placeholder:py-2 border-2 rounded-lg placeholder:px-3 py-2 placeholder:text-xs outline-none h-full"
 						placeholder="Paste your JSON code here"
 						on:input={handleJsonChange}
 						bind:value={JsonObject}
 						on:input={handleJsonChange}
 					/>
 				</form>
-				<div class="px-3 py-2 text-gray-700 border-2 rounded-lg bg-white mb-2">
-					<div class="flex flex-col justify-center">
-						<div class="flex">
+				<div class="px-20 py-2 text-gray-700 border-2 rounded-lg bg-cyan-800 mb-2">
+					<div class="flex flex-col justify-center gap-y-2 py-6">
+						<div
+							class="flex border-yellow-500 px-2 py-2 border-2 text-md rounded-md text-yellow-500 transform hover:scale-105 bg-white  transition duration-500 cursor-pointer"
+						>
+							<div class="px-2">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-4  w-4 fill-white "
+									fill="currentColor"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									stroke-width="2"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+									/>
+								</svg>
+							</div>
 							<h6 class="text-xs">Basic JSON validation</h6>
 							<!-- {console.log({ asdad: isJsonObjectValid })} -->
-							<p class="pl-4 text-xs">{isJsonObjectValid}</p>
+							<!-- <p class="pl-4 text-xs">{isJsonObjectValid}</p> -->
 						</div>
-						<div>
+						<div
+							class="flex border-red-500 px-2 py-2 border-2 text-md rounded-md text-red-500 transform hover:scale-105 bg-white  transition duration-500 cursor-pointer"
+						>
+							<div class="px-2">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-4  w-4 fill-white "
+									fill="currentColor"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									stroke-width="2"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+									/>
+								</svg>
+							</div>
 							<h6 class="text-xs">Removed squared brackets</h6>
+							<!-- {console.log({ asdad: isJsonObjectValid })} -->
+							<!-- <p class="pl-4 text-xs">{isJsonObjectValid}</p> -->
 						</div>
+						<img
+							src="https://media.giphy.com/media/3oEjI9xj49ehuAGLQY/giphy.gif"
+							class="rounded-md"
+							alt="code"
+						/>
 					</div>
 				</div>
 			</div>
